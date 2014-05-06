7f0a5e7913a758ec9ee4baef4dc1ad721bc73436
hasufell <hasufell@hasufell.de>
2014-05-06 23:18:38 +0200

    Alphabetical order for includes
    unless needed otherwise

--- parser.c
+++ parser.c
@@ -1,14 +1,14 @@
 #include "err.h"
 #include "types.h"
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 
 /*
