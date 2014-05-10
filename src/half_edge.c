c5c6c8b89aefecb13ab8c363c5f0076d367f084d
hasufell <hasufell@hasufell.de>
2014-05-10 19:16:39 +0200

    Add half_edge.c for operations on half edge structures

---
+++ src/half_edge.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2011-2014 hasufell
+ *
+ * This file is part of a hasufell project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "half_edge.h"
+
+#include <stdlib.h>
+
+
+/**
+ * Get all edges that emanate from vertice.
+ *
+ * @param vertice the vertice to get the emanating edges of
+ * @return pointer to a NULL-terminated array of half-edges
+ */
+HE_edge **get_all_emanating_edges(HE_vert *vertice)
+{
+	return NULL;
+}
